b9e7f7c0141a1e70bf25c6a5983ce21d2e74de43
Abbo <pedronicolasabba@hotmail.com>
2019-08-23 03:46:24 -0300

    Gem Device install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users, skip: [:registrations]
+    as :user do
+      get "/login" => "devise/sessions#new" # custom path to login/sign_in
+      get "/register" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+      get 'perfil' => 'devise/registrations#edit', :as => 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
 end
