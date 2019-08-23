5f215cf0b2ba5d553cad7a97a1d8bc7bbe16c2ff
Abbo <pedronicolasabba@hotmail.com>
2019-08-23 16:17:29 -0300

    Add namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users, skip: [:registrations]
-    as :user do
-      get "/login" => "devise/sessions#new" # custom path to login/sign_in
-      get "/register" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-      get 'perfil' => 'devise/registrations#edit', :as => 'edit_user_registration'
-      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  namespace :admin do
+    devise_for :user, controllers: { 
+      registrations: 'admin/user/registrations',
+      sessions: 'admin/user/sessions'
+    }
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
