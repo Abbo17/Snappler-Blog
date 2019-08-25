38295cbf56ae9eefab621c839ece11002f3eec7a
Abbo <pedronicolasabba@hotmail.com>
2019-08-25 19:36:12 -0300

    Modify Views

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,13 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resource :base, only: :index
-    
-    devise_for :user, controllers: { 
-      registrations: 'admin/user/registrations',
-      sessions: 'admin/user/sessions'
-    }
     resources :products do
     end
     root to: 'products#index'
   end
   
+  devise_for :users, path: 'admin'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
